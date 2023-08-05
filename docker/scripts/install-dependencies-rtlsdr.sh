0263be217e7eca753a10bdde3843d3b7900ee7db
Chris Tawfik <toughiq@gmail.com>
2023-08-05 23:35:27 +0200

    Update install-dependencies-rtlsdr.sh
    install official rtl-sdr

--- docker/scripts/install-dependencies-rtlsdr.sh
+++ docker/scripts/install-dependencies-rtlsdr.sh
@@ -24,9 +24,10 @@ BUILD_PACKAGES="git libusb-1.0.0-dev cmake make gcc g++ pkg-config"
 apt-get update
 apt-get -y install --no-install-recommends $STATIC_PACKAGES $BUILD_PACKAGES
 
-git clone https://github.com/osmocom/rtl-sdr.git
-# latest from master as of 2020-09-04
-cmakebuild rtl-sdr ed0317e6a58c098874ac58b769cf2e609c18d9a5
+#git clone https://github.com/osmocom/rtl-sdr.git
+## latest from master as of 2020-09-04
+#cmakebuild rtl-sdr ed0317e6a58c098874ac58b769cf2e609c18d9a5
+apt-get install -y rtl-sdr
 
 apt-get -y purge --autoremove $BUILD_PACKAGES
 apt-get clean
